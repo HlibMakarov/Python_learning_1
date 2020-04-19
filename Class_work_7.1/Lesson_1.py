6376df5ccd86c7f696e0e482a97ac4388cbdd1df
makarovhlib@gmail.com <makarovhlib@gmail.com>
2020-04-19 14:39:52 +0300

    start1

---
+++ Class_work_7.1/Lesson_1.py
@@ -0,0 +1,19 @@
+
+import math
+#print("Don`t Panic")
+length = 10
+width = 20
+rectangle_square = length * width
+print("площадь треугольника при ширине %d см. и длине %d см. равна: %d кв. см." %
+      (length, width, rectangle_square))
+
+catheter1 = 4
+catheter2 = 4
+hypotenuse = math.sqrt(catheter1**2 + catheter2**2)
+print("Длинна гипотенузы равна: %.2f см. при длинне катетов %d и %d см. "
+      %(hypotenuse, catheter1, catheter2))
+
+radius = 42000
+pi = math.pi
+area_of_a_circle = (radius**2) * pi
+print("Площадь круга равна: %5.2f см2 при радусе %.2f см" %(area_of_a_circle, radius))
